12687cbbba4d54e3e6c9bd9191c553fb20180fbf
Samuel Ortega <samy9093@gmail.com>
2023-01-18 21:05:42 -0600

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Sammy on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
